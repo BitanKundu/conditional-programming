460744b7918eca93e2b2418f60e8102189d91c08
BitanKundu <79818809+BitanKundu@users.noreply.github.com>
2021-11-06 12:46:20 -0700

    Add files via upload

---
+++ 100.py
@@ -0,0 +1,22 @@
+## conditional programming
+
+## to find the greater among two numbers
+a = 5
+b = 10
+if(a > b):
+    print(" a is the greate one")
+else:
+    print(" b is greater")
+
+
+
+d=30 
+
+e=40
+if( d>e):
+    print(" d is the greater one")
+else:
+    print("e is the greater one)
+
+
+
